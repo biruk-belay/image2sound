95aba47f7642d58a866c4173dfa17b5494c6195a
Biruk Seyoum <seyoum@fbk.eu>
2018-06-03 15:24:46 +0200

    added rgb

--- src/extract_rgb.c
+++ src/extract_rgb.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "jpeglib.h"
+#include "extract_rgb.h"
 
 #define PIXEL_COMPONENTS 3
 
-typedef struct{
-    int width;
-    int height;
-}image_size;
-
-int read_JPEG_file (char *filename, unsigned char *buffer);
+int read_JPEG_file (char *filename, size_t size, unsigned char *buffer);
 void copy_to_buffer(unsigned char *buffer, int row, unsigned char *data);
+void put_scanline_someplace(unsigned char *buffer, int row, FILE *f);
 
 /*This function reads the size of the image from the header and copies 
   the value in an image_size struct which is passed as a parameter
@@ -50,46 +48,55 @@ int get_image_size(char *filename, image_size *size)
    and copies the extracted RGB into the input buffer passed as a parameter.
 */
 
-int extract_rgb(char *filename, unsigned char *buffer)
+int extract_rgb(char *filename, size_t size, unsigned char *buffer)
 {
     int ret_val;
+    int i;
+   
+    assert(buffer != NULL);
+    unsigned char *temp_ptr = buffer;
 
-
-    if((ret_val = read_JPEG_file(filename, buffer) != 1)) {
+    if((ret_val = read_JPEG_file(filename, size, temp_ptr) != 1)) {
         fprintf(stderr, "RGB not extracted \n");
         return 0;
     }
-            
+ 
     return 1;
 }
 
-int read_JPEG_file (char *filename, unsigned char *data)
+int read_JPEG_file(char *filename, size_t size, unsigned char *data)
 {
-  /* This struct contains the JPEG decompression parameters and pointers to
-   * working space (which is allocated as needed by the JPEG library).
-   */
+    /* This struct contains the JPEG decompression parameters and pointers to
+    * working space (which is allocated as needed by the JPEG library).
+    */
   
     struct jpeg_decompress_struct cinfo;
     struct jpeg_error_mgr jerr;
-
-  /* More variables */
-    FILE * infile;         	/* source file */
+    
+    /* More variables */
+    FILE *infile;         	/* source file */
+    FILE *destfile;
     JSAMPARRAY img_buffer;        /* Output row buffer */
     int row_stride;       	/* physical row width in output buffer */
- 
+    int i = 0;
+
+    unsigned char *temp_ptr = data;
+
     if ((infile = fopen(filename, "rb")) == NULL) {
         fprintf(stderr, "read_JPEG_FILE: can't open %s\n", filename);
         return 0;
     }
-  
-    cinfo.err = jpeg_std_error(&jerr);
 
+    if ((destfile = fopen("decompressed_2", "w")) == NULL) {
+        fprintf(stderr, "read_JPEG_FILE: can't open %s\n", filename);
+        return 0;
+    }
+
+    cinfo.err = jpeg_std_error(&jerr);
     jpeg_create_decompress(&cinfo);
     jpeg_stdio_src(&cinfo, infile);
-    printf("everything well \n");
     (void) jpeg_read_header(&cinfo, TRUE);
     (void) jpeg_start_decompress(&cinfo); /* start decompressor */
-    printf("rows is %i and column is %i \n", cinfo.output_width, cinfo.output_height); 
 
     /* JSAMPLEs per row in output buffer */
     row_stride = cinfo.output_width * cinfo.output_components;
@@ -97,36 +104,40 @@ int read_JPEG_file (char *filename, unsigned char *data)
     img_buffer = (*cinfo.mem->alloc_sarray)
         ((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
 
-  /* Step 6: while (scan lines remain to be read) */
-  /*           jpeg_read_scanlines(...); */
+    /* Step 6: while (scan lines remain to be read) */
+    /*           jpeg_read_scanlines(...); */
 
-  /* Here we use the library's state variable cinfo.output_scanline as the
-   * loop counter, so that we don't have to keep track ourselves.
-   */
-  while (cinfo.output_scanline < cinfo.output_height) {
+    /* Here we use the library's state variable cinfo.output_scanline as the
+    * loop counter, so that we don't have to keep track ourselves.
+    */
+    while (cinfo.output_scanline < cinfo.output_height) {
     /* jpeg_read_scanlines expects an array of pointers to scanlines.
      * Here the array is only one element long, but you could ask for
      * more than one scanline at a time if that's more convenient.
      */
-    (void) jpeg_read_scanlines(&cinfo, img_buffer, 1);
+      
+        (void) jpeg_read_scanlines(&cinfo, img_buffer, 1);
     /* Assume put_scanline_someplace wants a pointer and sample count. */
-	copy_to_buffer(img_buffer[0], row_stride, data);
-  }
-  
-   // printf("rows is %i and column is %i \n", cinfo.output_width, cinfo.output_height); 
+	    copy_to_buffer(img_buffer[0], row_stride, temp_ptr+row_stride*i);
+        i++;
+    put_scanline_someplace(img_buffer[0], row_stride, destfile);
+    }
+ 
     (void) jpeg_finish_decompress(&cinfo);/* Step 7: Finish decompression */
     jpeg_destroy_decompress(&cinfo); /*Release JPEG decompression object */
     fclose(infile);
     return 1;
 }
 
-
 void copy_to_buffer(unsigned char *buffer, int row, unsigned char *data)
 {
     int i;
-    for(i = 0; i<row; i++)
-      *data++ = buffer[i];
+    //printf("ptr is %p \n", data);
+    for(i = 0; i<row; i++){
+        *data++ = buffer[i];        
+    }
 }
+
 /*
 void main()
 {
@@ -142,3 +153,10 @@ void main()
 
     //printf(" width is %i and height is %i \n", size.width, size.height);
 }*/
+
+void put_scanline_someplace(unsigned char *buffer, int row, FILE *f)
+{
+    int i; 
+    for(i = 0; i<row; i++)
+        fputc(buffer[i], f);
+}
---
+++ src/rgb_to_freq.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "extract_rgb.h"
+#include "rgb_to_freq.h"
+
+#define PIXEL_COMPONENTS 3
+#define MAX(x, y, z) x>y ? x>z ? x : z : y>z ? y : z
+#define MIN(x, y, z) x<y ? x<z ? x : z : y<z ? y : z
+
+int rgb_to_hsb( int red, int green, int blue, float *hue, float *sat, float *bright);
+
+int rgb_to_freq(unsigned char *pixel_data, float *freq_data, image_size size)
+{
+    unsigned char *rgb_pixel = pixel_data;
+    float *freq_amp_map = freq_data;
+    int i, j, k;
+    float hue, saturation, brightness;
+    
+    for(i=0, k=0; i<(size.width*size.height * 3); i+=3, k+=2) {
+        rgb_to_hsb(*(rgb_pixel+i), *(rgb_pixel+i+1), *(rgb_pixel+i+2),
+                &hue, &saturation, &brightness);
+       
+        if(hue == -1)
+            *(freq_amp_map+k) = FREQ_1;
+
+        else if(hue == -2)
+           *(freq_amp_map+k) = FREQ_14;
+
+        if(hue > 0 && hue <= 0.5) 
+           *(freq_amp_map+k) = FREQ_2;
+        
+        else if(hue > 0.5 && hue <= 1)
+           *(freq_amp_map+k) = FREQ_3;
+        
+        else if(hue > 1.0 && hue <= 1.5)
+            *(freq_amp_map+k) = FREQ_4;
+        
+        else if(hue > 1.5 && hue <= 2.0)
+            *(freq_amp_map+k) = FREQ_5; 
+        
+        else if(hue > 1.0 && hue <= 2.5)
+            *(freq_amp_map+k) = FREQ_6;
+        
+        else if(hue > 2.5 && hue <= 3.0)
+            *(freq_amp_map+k) = FREQ_7;
+        
+        else if(hue > 3.0 && hue <= 3.5)
+            *(freq_amp_map+k) = FREQ_8;
+        
+        else if(hue > 3.5 && hue <= 4.0)
+            *(freq_amp_map+k) = FREQ_9;
+        
+        else if(hue > 4.0 && hue <= 4.5)
+            *(freq_amp_map+k) = FREQ_10;
+        
+        else if(hue > 4.5 && hue <= 5.0)
+            *(freq_amp_map+k) = FREQ_11;
+        
+        else if(hue > 5.0 && hue <= 5.5)
+            *(freq_amp_map+k) = FREQ_12;
+        else 
+            *(freq_amp_map+k) = FREQ_13;
+
+        *(freq_amp_map+k+1) = brightness * saturation;
+        
+            //printf("freq and amplitude equal %i\n", i);    
+       printf("in rgb to freq %f %f \n", hue, brightness * saturation);
+    }
+    return 1;
+}
+
+int rgb_to_hsb( int red, int green, int blue, float *hue, float *sat, float *bright)
+{
+    float min, max, delta;
+    
+    assert((red <= 255)  || (red >= 0)   || 
+          (green <= 255) || (green >= 0) || 
+          (blue <= 255)  || (blue >= 0));
+    
+    float r = (float) red/255.0;
+    float b = (float) blue/255.0;
+    float g = (float) green/255.0;
+
+    min = MIN(r, b, g);
+    max = MAX(r, b, g);
+    
+    //printf("max and min %f %f \n", max, min);
+    *bright = max;
+    delta = max - min;
+   
+    if(delta == 0 && max != 0){
+        *hue = -2;
+        *sat = max;       // s
+        return 0;
+    }
+    else if(delta == 0 && max == 0) {
+        // r = g = b = 0        // s = 0, v is undefined
+        *sat = 1;
+        *bright = 1;
+        *hue = -1;
+        return 0;
+    }
+    else
+        *sat = delta / max;
+
+    if(r == max)
+        *hue = ( g - b ) / delta;     // between yellow & magenta
+    else if(g == max)
+        *hue = 2 + ( b - r ) / delta; // between cyan & yellow
+    else if (b == max)
+        *hue = 4 + ( r - g ) / delta; // between magenta & cyan 
+    else 
+        *hue = -2; 
+    
+        //*hue*= 60;               // degrees
+    //if( *hue < 0 )
+      //  *hue += 360;
+    
+    printf("in rgb to hsb %f %i %f %i %f %i %f %f  \n", r,red ,b ,blue,g ,green, min, max);    
+    return 1;
+}
+
+void main()                                                                               
+{
+    image_size img_size;
+    unsigned int pixel_buff_size, freq_buff_size;
+    unsigned int i;
+
+    get_image_size("input_9.jpg", &img_size);
+    pixel_buff_size = img_size.width * img_size.height * PIXEL_COMPONENTS;
+    freq_buff_size = img_size.height * img_size.width * 2;
+
+    printf(" width is %i and height is %i \n", img_size.width, img_size.height);      
+    printf("here \n"); 
+    printf(" pixel size is %i \n", pixel_buff_size);      
+
+    unsigned char buffer [pixel_buff_size];
+    unsigned char *buff_ptr = buffer;
+    float freq_mapping[freq_buff_size];
+    
+    printf("ptr is %p \n", buff_ptr);
+    extract_rgb("input_9.jpg", pixel_buff_size, buff_ptr);
+   
+    rgb_to_freq(buffer, freq_mapping, img_size);
+} 

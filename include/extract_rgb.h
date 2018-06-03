b01aa61112c3b7ed999f91c5d9395e20454811e8
Biruk Seyoum <seyoum@fbk.eu>
2018-06-03 15:26:14 +0200

    completed rgb to hsb to frequency mapping

--- include/extract_rgb.h
+++ include/extract_rgb.h
@@ -1,2 +1,10 @@
+
+typedef struct{
+    int width;
+    int height;
+}image_size;
+
 int get_image_size(char *filename, image_size *size);
-int extract_rgb(char *filename, unsigned char *buffer);
+//int read_JPEG_file(char *filename, size_t size, unsigned char *buffer);
+int extract_rgb(char *filename, size_t size, unsigned char *buffer);
+//int read_JPEG_file(char *filename, size_t size, unsigned char (*data)[size]);
---
+++ include/rgb_to_freq.h
@@ -0,0 +1,21 @@
+//#include "extract_rgb.h"
+
+enum FREQ_LEVLS {                                                                         
+    FREQ_1, 
+    FREQ_2 = 3,
+    FREQ_3,
+    FREQ_4,                                                                               
+    FREQ_5,                                                                               
+    FREQ_6,
+    FREQ_7,
+    FREQ_8,
+    FREQ_9,                                                                               
+    FREQ_10,                                                                              
+    FREQ_11,
+    FREQ_12,
+	FREQ_13,
+	FREQ_14 = 18,
+};
+
+//int rgb_to_hsb( int red, int green, int blue, float *hue, float *sat, float *bright);
+int rgb_to_freq(unsigned char *pixel_data, float *freq_data, image_size size);
